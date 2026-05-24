6fd61838ad7e1dad61748c493bb45bad83595290
GD-Rowl <myoung4965@gmail.com>
2026-05-24 14:06:33 +0900

    Add dummy game in build.sh
    스코어 기능을 테스트하기 위해 더미 게임을 만들고 build.sh 파일에 연결

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 set -e
 cd "$(dirname "$0")/.."
 
-mkdir -p bin data
+mkdir -p bin data games
 sh scripts/init.sh >/dev/null
 
 CC=${CC:-gcc}
@@ -14,3 +14,9 @@ SRC="src/lobby.c src/account.c src/score.c"
 echo "[BUILD] $CC $CFLAGS"
 $CC $CFLAGS $SRC -o bin/lobby
 echo "[OK] bin/lobby 생성 완료"
+
+# 2. 게임 빌드 산출물 위치
+if [ -f src/game1.c ]; then
+    $CC $CFLAGS src/game1.c -o games/game1
+    echo "[OK] games/game1 생성 완료"
+fi
